32df6ef9a579f1e43a810c0199a9e40bc9b18ae5
premiumajans <mmcvakant@gmail.com>
2023-08-10 12:25:22 +0400

    content single page

--- resources/views/frontend/includes/meta.blade.php
+++ resources/views/frontend/includes/meta.blade.php
@@ -1,5 +1,9 @@
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>@lang('backend.zuhur')</title>
-<meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>@yield('title') @lang('backend.zuhur')</title>
+@if(\Illuminate\Support\Facades\Route::currentRouteName() == 'frontend.selectedContent' )
+    @yield('meta')
+@else
+    <meta name="description" content="heleki testi">
+@endif
