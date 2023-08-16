86857c2f69ac9c837b028833ed9fb65e306a030a
premiumajans <mmcvakant@gmail.com>
2023-08-16 09:11:12 +0400

    info-not-found translation

--- resources/lang/ru/messages.php
+++ resources/lang/ru/messages.php
@@ -1,19 +1,18 @@
 <?php
 
-return [
-    'success' => 'Uğurla yerinə yetirildi!',
-    'error' => 'Əmiliyyat uğursuz oldu!',
-    'delete-success'=>'Uğurla silindi!',
-    'add-success' => 'Uğurla əlavə olundu!',
-    'send-success' => 'Uğurla göndərildi!',
-    'news' => 'Xəbərlər',
-    '404' => 'Təəssüf ki, səhifə tapılmadı',
-    '403' => 'Bu səhifəyə baxmaq üçün icazəniz yoxdur',
-    '500' => 'Daxili Server Xətası',
-    'not-admin-dashboard' => 'Təəssüf ki,bu məlumatlar sizin üçün əlçatan deyil!',
-    'newsletter' => 'E-poçt ünvanınızı daxil edin və yeniliklərdən xəbərdar olmaq üçün abunə olun.',
-    'info-not-found'=>'Təəssüf ki, məlumat tapılmadı.',
-    'is-correct' => 'düzgün daxil edildi!',
-    'not-correct' => 'düzgün daxil edilməmişdir!',
-
-];
+return array (
+  'add-success' => 'Uğurla əlavə olundu!',
+  'delete-success' => 'Uğurla silindi!',
+  'error' => 'Əmiliyyat uğursuz oldu!',
+  'info-not-found' => 'Извините, информация не найдена.',
+  'is-correct' => 'düzgün daxil edildi!',
+  'news' => 'Xəbərlər',
+  'newsletter' => 'E-poçt ünvanınızı daxil edin və yeniliklərdən xəbərdar olmaq üçün abunə olun.',
+  'not-admin-dashboard' => 'Təəssüf ki,bu məlumatlar sizin üçün əlçatan deyil!',
+  'not-correct' => 'düzgün daxil edilməmişdir!',
+  'send-success' => 'Uğurla göndərildi!',
+  'success' => 'Uğurla yerinə yetirildi!',
+  403 => 'Bu səhifəyə baxmaq üçün icazəniz yoxdur',
+  404 => 'Təəssüf ki, səhifə tapılmadı',
+  500 => 'Daxili Server Xətası',
+);
